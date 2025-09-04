4c9a49aace9f0547118fe335d9e90429aa73e474
vedamanikanta <manikanta.vedam@gmail.com>
2025-09-04 19:21:51 +0530

    class binding

--- src/app/test/test.ts
+++ src/app/test/test.ts
@@ -4,20 +4,44 @@ import { Component } from '@angular/core';
   selector: 'app-test',
   imports: [],
   template: `<h1>
-  Welcome to {{name}} Leanring Angular!
+    Welcome to {{name}} Leanring Angular!
   </h1>
   <input [id]="myId" type="text" value="VVM" />
   <input bind-disabled="isDisabled" id="{{myId}}" type="text" value="VVM" />
-
-  
+  <h1 class="text-success text-special">From VVM</h1>
+<h1 [class]="successClass">veda learning</h1>
+<h1 [class.text-danger]="hasError">veda learning</h1>
+  <h2 [class]="messageClasses">Anular 2025</h2>
   `,
-  styles: []
+  styles: [`
+    .text-success {
+      color: green;
+    }
+      .text-danger {
+      color: red;
+    }
+      .text-special {
+      font-style: italic;
+  }
+    `]
 })
 export class Test {
 public name  = 'Vedamanikanta';
 public url = window.location.href ;
 public myId = "testId";
 public isDisabled = false;
+
+// class binding
+public  successClass = "text-success";
+public hasError = true;
+public isSpecial = true;
+public messageClasses = {
+  "text-success": !this.hasError,
+  "text-danger": this.hasError,
+  "text-special": this.isSpecial
+};
+// class binding end
+
 greetUser(){
   return "Hi " + this.name;
 };
